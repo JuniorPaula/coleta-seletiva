ce5f2255a217859de782269f5ee13001bb10a2bf
Junior Paula <slucivaldo509@gmail.com>
2022-05-31 19:30:01 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/item-controller.ts
+++ src/presentation/controllers/item-controller.ts
@@ -1,19 +1,14 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helpers'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class ItemController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.title) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('title')
-      }
+      return badRequest(new MissingParamError('title'))
     }
     if (!httpRequest.body.image) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('image')
-      }
+      return badRequest(new MissingParamError('image'))
     }
   }
 }
