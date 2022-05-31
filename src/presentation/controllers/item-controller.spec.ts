83334a200af57e785d529a62a826ce4c06dd13f1
Junior Paula <slucivaldo509@gmail.com>
2022-05-31 19:14:52 -0300

    test: ensure ItemController returns 400 if no image is provided

--- src/presentation/controllers/item-controller.spec.ts
+++ src/presentation/controllers/item-controller.spec.ts
@@ -10,6 +10,18 @@ describe('Item Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: title'))
+  })
+
+  test('Should return 400 if no image is provided', async () => {
+    const sut = new ItemController()
+    const httpRequest = {
+      body: {
+        title: 'any_title'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: image'))
   })
 })
