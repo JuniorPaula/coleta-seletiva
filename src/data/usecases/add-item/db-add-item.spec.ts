1679db2726cfcb98516da89480c173e9d5df249b
Junior Paula <slucivaldo509@gmail.com>
2022-06-01 19:16:01 -0300

    test: move sut creation to a factory method

--- src/data/usecases/add-item/db-add-item.spec.ts
+++ src/data/usecases/add-item/db-add-item.spec.ts
@@ -3,22 +3,38 @@ import { AddItemModel } from '../../../domain/usecase/add-item'
 import { AddItemRepository } from '../../protocols/add-item-repository'
 import { DbAddItem } from './db-add-item'
 
+const makeAddItemRepository = (): AddItemRepository => {
+  class AddItemRepositoryStub implements AddItemRepository {
+    async add (itemData: AddItemModel): Promise<ItemModel> {
+      const fakeItem = {
+        id: 'valid_id',
+        title: 'valid_title',
+        image: 'valid_image'
+      }
+      return await new Promise(resolve => resolve(fakeItem))
+    }
+  }
+
+  return new AddItemRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddItem
+  addItemRepositoryStub: AddItemRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addItemRepositoryStub = makeAddItemRepository()
+  const sut = new DbAddItem(addItemRepositoryStub)
+  return {
+    sut,
+    addItemRepositoryStub
+  }
+}
+
 describe('DB AddItem usecase', () => {
   test('Should calls AddItemRepository with correct values', async () => {
-    class AddItemRepositoryStub implements AddItemRepository {
-      async add (itemData: AddItemModel): Promise<ItemModel> {
-        const fakeItem = {
-          id: 'valid_id',
-          title: 'valid_title',
-          image: 'valid_image'
-        }
-        return await new Promise(resolve => resolve(fakeItem))
-      }
-    }
-
-    const addItemRepositoryStub = new AddItemRepositoryStub()
-
-    const sut = new DbAddItem(addItemRepositoryStub)
+    const { sut, addItemRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addItemRepositoryStub, 'add')
     const itemData = {
       title: 'valid_title',
