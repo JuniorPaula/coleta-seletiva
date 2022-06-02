de17dfe2914d678feeb4be3c179363bbaa75494f
Junior Paula <slucivaldo509@gmail.com>
2022-06-02 19:43:13 -0300

    feat: add express-routes-adapter

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-routes-adapter'
+import { makeItemController } from '../factories/item'
 
 export default (router: Router): void => {
-  router.post('/item', (req, res) => {
-    res.json({ ok: 'fine' })
-  })
+  router.post('/item', adaptRoute(makeItemController()))
 }
