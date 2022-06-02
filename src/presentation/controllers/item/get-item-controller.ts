5945c93428b4c39af04ee18cb4fab819eab4450a
Junior Paula <slucivaldo509@gmail.com>
2022-06-02 20:41:07 -0300

    feat: ensure GetItemController throws if GetItem throws

--- src/presentation/controllers/item/get-item-controller.ts
+++ src/presentation/controllers/item/get-item-controller.ts
@@ -1,13 +1,18 @@
+import { serverError } from '../../helpers/http-helpers'
 import { HttpResponse } from '../../protocols'
 import { Controller, GetItem } from './item-protocols'
 
 export class GetItemsController implements Controller {
   constructor (private readonly getItem: GetItem) {}
   async handle (): Promise<HttpResponse> {
-    const items = await this.getItem.get()
-    return {
-      statusCode: 200,
-      body: items
+    try {
+      const items = await this.getItem.get()
+      return {
+        statusCode: 200,
+        body: items
+      }
+    } catch (error) {
+      return serverError()
     }
   }
 }
