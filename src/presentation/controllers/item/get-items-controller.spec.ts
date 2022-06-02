6e4085c33444aa06ac96351ef28d9399f19948dc
Junior Paula <slucivaldo509@gmail.com>
2022-06-02 20:35:41 -0300

    test: ensure GetItem calls once time

--- src/presentation/controllers/item/get-items-controller.spec.ts
+++ src/presentation/controllers/item/get-items-controller.spec.ts
@@ -1,10 +1,47 @@
 import { GetItemsController } from './get-item-controller'
+import { ItemModel, GetItem } from './item-protocols'
+
+const makeGetItem = (): GetItem => {
+  class GetItemStub implements GetItem {
+    async get (): Promise<ItemModel[]> {
+      const fakeItems = [
+        {
+          id: 'any_id',
+          title: 'any_title',
+          image: 'any_image'
+        },
+        {
+          id: 'another_id',
+          title: 'another_title',
+          image: 'another_image'
+        }
+      ]
+      return await new Promise(resolve => resolve(fakeItems))
+    }
+  }
+
+  return new GetItemStub()
+}
+
+interface SutTypes {
+  sut: GetItemsController
+  getItem: GetItem
+}
+
+const makeSut = (): SutTypes => {
+  const getItem = makeGetItem()
+  const sut = new GetItemsController(getItem)
+  return {
+    sut,
+    getItem
+  }
+}
 
 describe('Get Items Controllers', () => {
-  test('Should return an items on success', async () => {
-    const sut = new GetItemsController()
-    const httpResponse = await sut.handle()
-    expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual([])
+  test('Should calls Getitem once time', async () => {
+    const { sut, getItem } = makeSut()
+    const itemsSpy = jest.spyOn(getItem, 'get')
+    await sut.handle()
+    expect(itemsSpy).toBeCalledTimes(1)
   })
 })
