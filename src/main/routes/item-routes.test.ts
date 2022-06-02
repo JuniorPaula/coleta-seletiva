7a0069fea268ded6f8dee299b648ed6efc4a75f1
Junior Paula <slucivaldo509@gmail.com>
2022-06-02 18:40:42 -0300

    refactor: connect mongodb before integration test

--- src/main/routes/item-routes.test.ts
+++ src/main/routes/item-routes.test.ts
@@ -1,12 +1,25 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { app } from '../config/app'
 
 describe('Item Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const itemColletion = MongoHelper.getCollection('items')
+    await itemColletion.deleteMany({})
+  })
   test('Should return an item on success', async () => {
     await request(app)
       .post('/api/item')
       .send({
-        title: 'Olé de cozinha',
+        title: 'Oléo de cozinha',
         image: 'oleo.png'
       })
       .expect(200)
