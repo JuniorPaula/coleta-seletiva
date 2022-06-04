fe8a299b320bc00fce8d844ddec9cf5018d912a9
Junior Paula <slucivaldo509@gmail.com>
2022-06-03 21:11:53 -0300

    feat: ensure item router returns a colletion of items on success

--- src/main/factories/item.ts
+++ src/main/factories/item.ts
@@ -1,9 +1,17 @@
 import { DbAddItem } from '../../data/usecases/add-item/db-add-item'
+import { DbGetItem } from '../../data/usecases/get-item/db-get-item'
 import { ItemMongoRepository } from '../../infra/db/mongodb/item-repository/item'
+import { GetItemsController } from '../../presentation/controllers/item/get-item-controller'
 import { ItemController } from '../../presentation/controllers/item/item-controller'
 
 export const makeItemController = (): ItemController => {
   const itemMongoRepository = new ItemMongoRepository()
   const dbAddItem = new DbAddItem(itemMongoRepository)
   return new ItemController(dbAddItem)
+}
+
+export const makeGetItemController = (): GetItemsController => {
+  const itemMongoRepository = new ItemMongoRepository()
+  const dbGetItem = new DbGetItem(itemMongoRepository)
+  return new GetItemsController(dbGetItem)
 }
--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
-import { makeItemController } from '../factories/item'
+import { makeGetItemController, makeItemController } from '../factories/item'
 
 export default (router: Router): void => {
   router.post('/item', adaptRoute(makeItemController()))
+  router.get('/item', adaptRoute(makeGetItemController()))
 }
