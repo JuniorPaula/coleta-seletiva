fe49b907be8e27bbd311c4826c498d16dc80b4f6
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 10:08:16 -0300

    feat: ensure LocationMongoRepository create a relation of the locationItems on success

--- src/infra/db/mongodb/location-repository/location.ts
+++ src/infra/db/mongodb/location-repository/location.ts
@@ -1,11 +1,18 @@
+import { LocationItemRepository } from '@/data/protocols/location-item/location-item-repository'
 import { AddLocationRepository } from '@/data/protocols/locations/add-location-repository'
+import { LocationItemModel } from '@/domain/model/location-item-model'
 import { AddLocationModel } from '@/domain/usecases/locations/add-location'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class LocationMongoRepository implements AddLocationRepository {
+export class LocationMongoRepository implements AddLocationRepository, LocationItemRepository {
   async add (location: AddLocationModel): Promise<string> {
     const locationCollection = MongoHelper.getCollection('locations')
     const locationId = await locationCollection.insertOne(location)
     return locationId.insertedId.toHexString()
+  }
+
+  async create (data: LocationItemModel[]): Promise<void> {
+    const locationItemCollection = MongoHelper.getCollection('locations_items')
+    await locationItemCollection.insertOne({ data })
   }
 }
