38e3ab5130128064495f56df3771077ba28063ce
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 11:24:57 -0300

    test: ensure ValidationComposite not returns if validation succeeds

--- src/validations/validator/validation-composite.spec.ts
+++ src/validations/validator/validation-composite.spec.ts
@@ -41,4 +41,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not returns if validation succeeds', async () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
