0444a8c280b69a17e78ac699f12c724b3b2332be
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 11:38:04 -0300

    feat: ensure ItemValidationFactory calls ValidationComposite with all validations

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
-import { makeGetItemController, makeItemController } from '../factories/item'
+import { makeGetItemController, makeItemController } from '../factories/item/item'
 
 export default (router: Router): void => {
   router.post('/item', adaptRoute(makeItemController()))
