d7f343d0fe582ac9d8aa3a535af9cf5273293d5f
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 11:40:58 -0300

    refactor: add ItemValidationFactory in ItemController

--- src/main/factories/item/item.ts
+++ src/main/factories/item/item.ts
@@ -3,11 +3,12 @@ import { DbGetItem } from '@/data/usecases/items/get-item/db-get-item'
 import { ItemMongoRepository } from '@/infra/db/mongodb/item-repository/item'
 import { GetItemsController } from '@/presentation/controllers/item/get-item-controller'
 import { ItemController } from '@/presentation/controllers/item/item-controller'
+import { makeItemValidation } from './item-validation-factory'
 
 export const makeItemController = (): ItemController => {
   const itemMongoRepository = new ItemMongoRepository()
   const dbAddItem = new DbAddItem(itemMongoRepository)
-  return new ItemController(dbAddItem)
+  return new ItemController(dbAddItem, makeItemValidation())
 }
 
 export const makeGetItemController = (): GetItemsController => {
