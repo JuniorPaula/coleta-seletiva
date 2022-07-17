0d0546d244b78469a1cfccb5cb71e768b45dfec7
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 10:48:42 -0300

    feat: ensure LocationController calls AddLocation with correct values

--- src/presentation/controllers/location/location-controller.ts
+++ src/presentation/controllers/location/location-controller.ts
@@ -1,15 +1,29 @@
+import { AddLocation } from '@/domain/usecases/locations/add-location'
 import { badRequest } from '@/presentation/helpers/http-helpers'
 import { Validation } from '@/presentation/protocols/validation'
 import { Controller, HttpRequest, HttpResponse } from '../item/item-protocols'
 
 export class LocationController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addLocation: AddLocation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
+    const { name, email, latitude, longitude, city, uf, items } = httpRequest.body
+    await this.addLocation.add({
+      name,
+      email,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items
+    })
 
     return {
       statusCode: 200,
