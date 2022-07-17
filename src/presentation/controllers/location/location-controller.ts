db762f34000c77551fcaac800ec5f63e879c3758
Junior Paula <slucivaldo509@gmail.com>
2022-07-17 10:33:05 -0300

    feat: ensure LocationController returns 400 if Validation fails

--- src/presentation/controllers/location/location-controller.ts
+++ src/presentation/controllers/location/location-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers/http-helpers'
 import { Validation } from '@/presentation/protocols/validation'
 import { Controller, HttpRequest, HttpResponse } from '../item/item-protocols'
 
@@ -5,7 +6,11 @@ export class LocationController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
+
     return {
       statusCode: 200,
       body: null
