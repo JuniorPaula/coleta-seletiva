32235edf57c2f996a33423ece6ecdad84c69b995
Junior Paula <slucivaldo509@gmail.com>
2022-07-18 20:20:54 -0300

    test: ensure LocationMongoRepository returns locations on sucess

--- src/infra/db/mongodb/location-repository/location.spec.ts
+++ src/infra/db/mongodb/location-repository/location.spec.ts
@@ -71,4 +71,42 @@ describe('Location Mongo Repository', () => {
     const res = await locationsItemsCollection.find().toArray()
     expect(res).toBeTruthy()
   })
+
+  test('Should return a locations on success', async () => {
+    const { sut } = makeSut()
+    await locationCollection.insertMany([
+      {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        latitude: 12345,
+        longitude: 54321,
+        city: 'any_city',
+        uf: 'any_uf',
+        items: [
+          'any_item_id_1',
+          'any_item_id_2'
+        ]
+      },
+      {
+        name: 'other_name',
+        email: 'other_email@mail.com',
+        latitude: 12345,
+        longitude: 54321,
+        city: 'other_city',
+        uf: 'other_uf',
+        items: [
+          'other_item_id_1',
+          'other_item_id_2'
+        ]
+      }
+    ])
+    const locations = await sut.get({
+      city: 'any_city',
+      uf: 'any_uf',
+      items: ['any_item_id']
+    })
+    console.log(locations)
+    expect(locations[0].name).toEqual('any_name')
+    expect(locations[1].name).toEqual('other_name')
+  })
 })
