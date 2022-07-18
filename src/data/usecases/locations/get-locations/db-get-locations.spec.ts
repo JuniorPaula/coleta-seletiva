2cce7b99aa32f8e0ba76a960a6146faa14564e6d
Junior Paula <slucivaldo509@gmail.com>
2022-07-18 19:32:16 -0300

    test: move sut creation to a factory method

--- src/data/usecases/locations/get-locations/db-get-locations.spec.ts
+++ src/data/usecases/locations/get-locations/db-get-locations.spec.ts
@@ -1,6 +1,6 @@
 import { GetLocationsRepository } from '@/data/protocols/locations/get-locations-repository'
 import { LocationModel } from '@/domain/model/location-model'
-import { DataLocation } from '@/domain/usecases/locations/get-locations'
+import { DataLocation, GetLocations } from '@/domain/usecases/locations/get-locations'
 import { DbGetLocations } from './db-get-locations'
 
 const mockQuery = (): DataLocation => ({
@@ -11,28 +11,47 @@ const mockQuery = (): DataLocation => ({
   ]
 })
 
+const mockLocationModel = (): LocationModel => ({
+  id: 'any_id',
+  image: 'any_url_image',
+  name: 'any_name',
+  email: 'any_email',
+  latitude: 1234,
+  longitude: 5678,
+  city: 'any_city',
+  uf: 'any_uf',
+  items: [
+    'any_item_id'
+  ]
+})
+
+const mockGetLocationsRepository = (): GetLocationsRepository => {
+  class GetLocationsRepositoryStub implements GetLocationsRepository {
+    async get (query: DataLocation): Promise<LocationModel> {
+      return await Promise.resolve(mockLocationModel())
+    }
+  }
+  return new GetLocationsRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbGetLocations
+  getLocationsStub: GetLocations
+}
+
+const makeSut = (): SutTypes => {
+  const getLocationsStub = mockGetLocationsRepository()
+  const sut = new DbGetLocations(getLocationsStub)
+
+  return {
+    sut,
+    getLocationsStub
+  }
+}
+
 describe('Db GetLocations usecase', () => {
   test('Should calls GetLocationsRepository with correct values', async () => {
-    class GetLocationsRepositoryStub implements GetLocationsRepository {
-      async get (query: DataLocation): Promise<LocationModel> {
-        return await Promise.resolve({
-          id: 'any_id',
-          image: 'any_url_image',
-          name: 'any_name',
-          email: 'any_email',
-          latitude: 1234,
-          longitude: 5678,
-          city: 'any_city',
-          uf: 'any_uf',
-          items: [
-            'any_item_id'
-          ]
-        })
-      }
-    }
-
-    const getLocationsStub = new GetLocationsRepositoryStub()
-    const sut = new DbGetLocations(getLocationsStub)
+    const { sut, getLocationsStub } = makeSut()
     const getSpy = jest.spyOn(getLocationsStub, 'get')
     await sut.get(mockQuery())
     expect(getSpy).toHaveBeenCalledWith(mockQuery())
