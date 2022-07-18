a9432274d132e76bc5bbd85978fffebfef9792e6
Junior Paula <slucivaldo509@gmail.com>
2022-07-18 18:36:05 -0300

    refactor: change errors names to returns

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/index.ts'
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/**/**-protocols.ts'
   ],
   setupFiles: ['dotenv/config'],
   coverageDirectory: 'coverage',
--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,6 @@
 export class ServerError extends Error {
   constructor () {
     super('Internal server error')
-    this.name = 'ServerError'
+    this.name = 'Internal server error'
   }
 }
