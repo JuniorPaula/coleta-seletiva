3735ffeb1d75fec599ae9fdbd3ad9670100208de
Junior Paula <slucivaldo509@gmail.com>
2022-07-24 10:15:06 -0300

    test: move sut creation to a factory method

--- src/presentation/controllers/location/get-location-controller.spec.ts
+++ src/presentation/controllers/location/get-location-controller.spec.ts
@@ -7,31 +7,50 @@ const mockHttpRequest = (): HttpRequest => ({
   query: 'any_query'
 })
 
+const mockLocationModel = (): LocationModel => ({
+  location: {
+    id: 'any_id',
+    image: 'any_url_image',
+    name: 'any_name',
+    email: 'any_email',
+    latitude: 1234,
+    longitude: 5678,
+    city: 'any_city',
+    uf: 'any_uf'
+  },
+  items: [
+    { title: 'any_title' },
+    { title: 'other_title' }
+  ]
+})
+
+const mockGetLocation = (): GetLocations => {
+  class GetLocationStub implements GetLocations {
+    async get (query: DataLocation): Promise<LocationModel> {
+      return await Promise.resolve(mockLocationModel())
+    }
+  }
+  return new GetLocationStub()
+}
+
+type Suttypes = {
+  sut: GetLocationController
+  getLocationStub: GetLocations
+}
+
+const makeSut = (): Suttypes => {
+  const getLocationStub = mockGetLocation()
+  const sut = new GetLocationController(getLocationStub)
+
+  return {
+    sut,
+    getLocationStub
+  }
+}
+
 describe('GetLocation Controller', () => {
   test('Should call GetLocation with correct value', async () => {
-    class GetLocationStub implements GetLocations {
-      async get (query: DataLocation): Promise<LocationModel> {
-        const locations: LocationModel = {
-          location: {
-            id: 'any_id',
-            image: 'any_url_image',
-            name: 'any_name',
-            email: 'any_email',
-            latitude: 1234,
-            longitude: 5678,
-            city: 'any_city',
-            uf: 'any_uf'
-          },
-          items: [
-            { title: 'any_title' },
-            { title: 'other_title' }
-          ]
-        }
-        return await Promise.resolve(locations)
-      }
-    }
-    const getLocationStub = new GetLocationStub()
-    const sut = new GetLocationController(getLocationStub)
+    const { sut, getLocationStub } = makeSut()
     const getSpy = jest.spyOn(getLocationStub, 'get')
     const httpRequest = mockHttpRequest()
     await sut.handle(httpRequest)
