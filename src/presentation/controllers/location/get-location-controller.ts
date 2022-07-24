2f68c72dd4bf07dd287b2dec92cf0477a0a6fbb8
Junior Paula <slucivaldo509@gmail.com>
2022-07-24 11:22:49 -0300

    feat: ensure GetLocationController return 500 if GetLocation throws

--- src/presentation/controllers/location/get-location-controller.ts
+++ src/presentation/controllers/location/get-location-controller.ts
@@ -1,15 +1,19 @@
 import { GetLocations } from '@/domain/usecases/locations/get-locations'
-import { notFoundError } from '@/presentation/helpers/http-helpers'
+import { notFoundError, serverError } from '@/presentation/helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../item/item-protocols'
 
 export class GetLocationController implements Controller {
   constructor (private readonly getLocation: GetLocations) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const locations = await this.getLocation.get(httpRequest.query)
-    if (!locations.length) {
-      return notFoundError()
+    try {
+      const locations = await this.getLocation.get(httpRequest.query)
+      if (!locations.length) {
+        return notFoundError()
+      }
+      return await new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError()
     }
-    return await new Promise(resolve => resolve(null))
   }
 }
