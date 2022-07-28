b3c70113f0ca844d446b97f9b289b3b6b50807ec
Junior Paula <slucivaldo509@gmail.com>
2022-07-28 19:02:53 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/locations/load-location-by-id/db-load-location-by-id.spec.ts
+++ src/data/usecases/locations/load-location-by-id/db-load-location-by-id.spec.ts
@@ -1,33 +1,53 @@
 import { LoadLocationByIdRepository } from '@/data/protocols/locations/load-location-by-id-repository'
 import { LocationModel } from '@/domain/model/location-model'
+import { LoadLocationById } from '@/domain/usecases/locations/load-location-by-id'
 import { DbLoadLocationById } from './db-load-location-by-id'
+
+const mockLocationModel = (): LocationModel => ({
+  location: {
+    id: 'any_id',
+    image: 'any_url_image',
+    name: 'any_name',
+    email: 'any_email',
+    latitude: 1234,
+    longitude: 5678,
+    city: 'any_city',
+    uf: 'any_uf'
+  },
+  items: [
+    { title: 'any_title' },
+    { title: 'other_title' }
+  ]
+})
+
+const mockLoadLocationByIdRepository = (): LoadLocationByIdRepository => {
+  class LoadLocationByIdRepositoryStub implements LoadLocationByIdRepository {
+    async loadById (id: string): Promise<LocationModel> {
+      return await Promise.resolve(mockLocationModel())
+    }
+  }
+
+  return new LoadLocationByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadLocationById
+  loadLocationByIdRepositoryStub: LoadLocationById
+}
+
+const makeSut = (): SutTypes => {
+  const loadLocationByIdRepositoryStub = mockLoadLocationByIdRepository()
+  const sut = new DbLoadLocationById(loadLocationByIdRepositoryStub)
+
+  return {
+    sut,
+    loadLocationByIdRepositoryStub
+  }
+}
 
 describe('DB LoadLocationById usecase', () => {
   test('Should call LoadLocationByIdRepository with correct id', async () => {
-    class LoadLocationByIdRepositoryStub implements LoadLocationByIdRepository {
-      async loadById (id: string): Promise<LocationModel> {
-        return await Promise.resolve(
-          {
-            location: {
-              id: 'any_id',
-              image: 'any_url_image',
-              name: 'any_name',
-              email: 'any_email',
-              latitude: 1234,
-              longitude: 5678,
-              city: 'any_city',
-              uf: 'any_uf'
-            },
-            items: [
-              { title: 'any_title' },
-              { title: 'other_title' }
-            ]
-          }
-        )
-      }
-    }
-    const loadLocationByIdRepositoryStub = new LoadLocationByIdRepositoryStub()
-    const sut = new DbLoadLocationById(loadLocationByIdRepositoryStub)
+    const { sut, loadLocationByIdRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadLocationByIdRepositoryStub, 'loadById')
     await sut.loadById('location_id')
     expect(loadSpy).toHaveBeenCalledWith('location_id')
