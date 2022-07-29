6ba61ccfe5e630e5dd53336e57b6ed798d58b7ad
Junior Paula <slucivaldo509@gmail.com>
2022-07-29 19:21:41 -0300

    feat: ensure LoadLocationByIdController return 500 if LoadLocationById throws

--- src/presentation/controllers/location/load-location-by-id-controller.ts
+++ src/presentation/controllers/location/load-location-by-id-controller.ts
@@ -1,19 +1,24 @@
 import { LoadLocationById } from '@/domain/usecases/locations/load-location-by-id'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbiden } from '@/presentation/helpers/http-helpers'
+import { forbiden, serverError } from '@/presentation/helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../item/item-protocols'
 
 export class LoadLocationByIdController implements Controller {
   constructor (private readonly loadLocationById: LoadLocationById) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { locationId } = httpRequest.params
-    const location = await this.loadLocationById.loadById(locationId)
-    if (!location) {
-      return forbiden(new InvalidParamError('locationId'))
-    }
-    return {
-      statusCode: 200,
-      body: 'ok'
+    try {
+      const { locationId } = httpRequest.params
+      const location = await this.loadLocationById.loadById(locationId)
+      if (!location) {
+        return forbiden(new InvalidParamError('locationId'))
+      }
+      return {
+        statusCode: 200,
+        body: 'ok'
+      }
+    } catch (error) {
+      return serverError()
     }
   }
 }
