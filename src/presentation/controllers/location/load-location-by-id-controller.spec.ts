1e1a204ac1ced983855e4846da922f8530203c6d
Junior Paula <slucivaldo509@gmail.com>
2022-07-29 19:07:49 -0300

    feat: move sut creation to a factory method

--- src/presentation/controllers/location/load-location-by-id-controller.spec.ts
+++ src/presentation/controllers/location/load-location-by-id-controller.spec.ts
@@ -3,37 +3,58 @@ import { LoadLocationById } from '@/domain/usecases/locations/load-location-by-i
 import { HttpRequest } from '@/presentation/protocols'
 import { LoadLocationByIdController } from './load-location-by-id-controller'
 
+const mockLocationModel = (): LocationModel => ({
+  location: {
+    id: 'any_id',
+    image: 'any_url_image',
+    name: 'any_name',
+    email: 'any_email',
+    latitude: 1234,
+    longitude: 5678,
+    city: 'any_city',
+    uf: 'any_uf'
+  },
+  items: [
+    { title: 'any_title' },
+    { title: 'other_title' }
+  ]
+})
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    locatonId: 'location_id'
+  }
+})
+
+const mockLoadLocationById = (): LoadLocationById => {
+  class LoadLocationByIdStub implements LoadLocationById {
+    async loadById (id: string): Promise<LocationModel> {
+      return await Promise.resolve(mockLocationModel())
+    }
+  }
+  return new LoadLocationByIdStub()
+}
+
+type SutTypes = {
+  sut: LoadLocationByIdController
+  loadLocationByIdStub: LoadLocationById
+}
+
+const makeSut = (): SutTypes => {
+  const loadLocationByIdStub = mockLoadLocationById()
+  const sut = new LoadLocationByIdController(loadLocationByIdStub)
+
+  return {
+    sut,
+    loadLocationByIdStub
+  }
+}
+
 describe('LoadLocationById Controller', () => {
   test('Should call LoadLocationById with correct id', async () => {
-    class LoadLocationByIdStub implements LoadLocationById {
-      async loadById (id: string): Promise<LocationModel> {
-        return await Promise.resolve({
-          location: {
-            id: 'any_id',
-            image: 'any_url_image',
-            name: 'any_name',
-            email: 'any_email',
-            latitude: 1234,
-            longitude: 5678,
-            city: 'any_city',
-            uf: 'any_uf'
-          },
-          items: [
-            { title: 'any_title' },
-            { title: 'other_title' }
-          ]
-        })
-      }
-    }
-    const loadLocationByIdStub = new LoadLocationByIdStub()
-    const sut = new LoadLocationByIdController(loadLocationByIdStub)
+    const { sut, loadLocationByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadLocationByIdStub, 'loadById')
-    const httpRequest: HttpRequest = {
-      params: {
-        locatonId: 'location_id'
-      }
-    }
-    await sut.handle(httpRequest)
+    await sut.handle(mockRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('location_id')
   })
 })
