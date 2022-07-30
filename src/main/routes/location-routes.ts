0855e746a5719ce6143c1b998b788b98904aad5f
Junior Paula <slucivaldo509@gmail.com>
2022-07-30 15:35:06 -0300

    feat: ensure location routes return a location by id

--- src/main/routes/location-routes.ts
+++ src/main/routes/location-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeGetLocationController } from '../factories/location/get-location'
+import { makeLoadLocationByIdController } from '../factories/location/load-location-by-id-factory'
 import { makeLocationController } from '../factories/location/location'
 
 export default (router: Router): void => {
   router.post('/location', adaptRoute(makeLocationController()))
   router.get('/location', adaptRoute(makeGetLocationController()))
+  router.get('/location/:locationId', adaptRoute(makeLoadLocationByIdController()))
 }
