d0606347bd1c45739e7d8b9c5e7d1db2cd1ca3f1
Junior Paula <slucivaldo509@gmail.com>
2022-08-02 19:01:40 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/account/db-add-account/db-add-account.spec.ts
+++ src/data/usecases/account/db-add-account/db-add-account.spec.ts
@@ -1,21 +1,42 @@
+import { Encrypter } from '@/data/protocols/criptography/encrypter'
+import { AccountModel } from '@/domain/model/account'
 import { DbAddAccount } from './db-add-account'
+
+const mockAccount = (): Omit<AccountModel, 'id'> => ({
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'valid_password'
+})
+
+const mockEncrypter = (): Encrypter => {
+  class EncrypterStub {
+    async encrypt (email: string): Promise<string> {
+      return await Promise.resolve('encrypted_password')
+    }
+  }
+  return new EncrypterStub()
+}
+
+type SutTypes = {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = mockEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (email: string): Promise<string> {
-        return await Promise.resolve('encrypted_password')
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
-    const account = {
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'valid_password'
-    }
-    await sut.create(account)
+    await sut.create(mockAccount())
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 })
