135f2f9b64900c98bce12bcec1d841310bdedfcb
Junior Paula <slucivaldo509@gmail.com>
2022-08-05 19:17:23 -0300

    feat: create signup routes

---
+++ src/main/routes/account-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-routes-adapter'
+import { makeSignupController } from '../factories/account/signup'
+
+export default (router: Router): void => {
+  router.post('/signup', adaptRoute(makeSignupController()))
+}
