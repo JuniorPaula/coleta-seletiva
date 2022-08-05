512bf089ced325f2b8ea85058681bf28e2e96163
Junior Paula <slucivaldo509@gmail.com>
2022-08-05 18:48:43 -0300

    test: ensure AccountMongoRepository returns an account inserted on success

---
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+let accountColletion: Collection
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountColletion = MongoHelper.getCollection('accounts')
+    await accountColletion.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const accountModel = {
+      name: 'Jane Doe',
+      email: 'jane@mail.com',
+      password: '1234'
+    }
+    const account = await sut.add(accountModel)
+    expect(account.name).toBe('Jane Doe')
+    expect(account.email).toBe('jane@mail.com')
+    expect(account.password).toBe('1234')
+  })
+})
