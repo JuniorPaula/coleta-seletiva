ec448f82528749e17a71b299eb9f06dde506ad09
Junior Paula <slucivaldo509@gmail.com>
2022-08-06 12:09:05 -0300

    test: ensure CompareFieldsValidation not returns if validation succeeds

--- src/validations/validator/compare-field-validation.spec.ts
+++ src/validations/validator/compare-field-validation.spec.ts
@@ -11,4 +11,13 @@ describe('CompareFields Validation', () => {
 
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
+
+  test('Should not returns if validation succeeds', async () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
 })
