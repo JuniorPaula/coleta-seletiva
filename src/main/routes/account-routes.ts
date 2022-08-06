0bc4d8e7c68368c0721a69663c37491536a21ad1
Junior Paula <slucivaldo509@gmail.com>
2022-08-06 11:51:16 -0300

    refactor: move required fild validation to ValidationComposite

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
-import { makeSignupController } from '../factories/account/signup'
+import { makeSignupController } from '../factories/account/signup/signup'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
