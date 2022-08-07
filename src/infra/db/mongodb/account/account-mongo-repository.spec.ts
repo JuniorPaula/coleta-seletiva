901ff3d81f9fa28e458974d80960796b20c68375
Junior Paula <slucivaldo509@gmail.com>
2022-08-07 15:41:36 -0300

    test: ensure AccountMongoRepository return null if loadByEmail fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -50,5 +50,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('jane@mail.com')
       expect(account.password).toBe('1234')
     })
+
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('jane@mail.com')
+      expect(account).toBeFalsy()
+    })
   })
 })
