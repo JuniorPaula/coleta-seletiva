34c6e698d181e1ee7914e3f37e8d087b83581eb6
Junior Paula <slucivaldo509@gmail.com>
2022-08-07 15:05:24 -0300

    feat: ensure JwtAdapter return a token on sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -5,7 +5,7 @@ export class JwtAdapter implements TokenGeneration {
   constructor (private readonly secret: string) {}
 
   async generate (id: string): Promise<string> {
-    jwt.sign({ id }, this.secret)
-    return null
+    const accessToken = jwt.sign({ id }, this.secret)
+    return accessToken
   }
 }
