b67af2b1ac7870b0d1a0cfb70f206719489487b7
Junior Paula <slucivaldo509@gmail.com>
2022-08-07 15:09:31 -0300

    refactor: move sign tests to a owner describe

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,25 +8,27 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('JwtAdapter', () => {
-  test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.generate('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
-  })
-
-  test('Should return a token on sign succeeds', async () => {
-    const sut = new JwtAdapter('secret')
-    const accessToken = await sut.generate('any_id')
-    expect(accessToken).toBe('access_token')
-  })
-
-  test('Should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error()
+  describe('sign()', () => {
+    test('Should call sign with correct values', async () => {
+      const sut = new JwtAdapter('secret')
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.generate('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     })
-    const promise = sut.generate('any_id')
-    await expect(promise).rejects.toThrow()
+
+    test('Should return a token on sign succeeds', async () => {
+      const sut = new JwtAdapter('secret')
+      const accessToken = await sut.generate('any_id')
+      expect(accessToken).toBe('access_token')
+    })
+
+    test('Should throw if sign throws', async () => {
+      const sut = new JwtAdapter('secret')
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.generate('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
