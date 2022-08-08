6d573ccabe74d2d61c98cdca1556101a0287eb4a
Junior Paula <slucivaldo509@gmail.com>
2022-08-08 19:34:18 -0300

    refactor: rename acount routes to login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -5,7 +5,7 @@ import app from '../config/app'
 
 let accountColletion: Collection
 
-describe('Account Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -18,15 +18,18 @@ describe('Account Routes', () => {
     accountColletion = MongoHelper.getCollection('accounts')
     await accountColletion.deleteMany({})
   })
-  test('Should return 200 on signup', async () => {
-    await request(app)
-      .post('/api/v1/signup')
-      .send({
-        name: 'Jane Doe',
-        email: 'jane@mail.com',
-        password: '1234',
-        passwordConfirmation: '1234'
-      })
-      .expect(200)
+
+  describe('POST/signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/v1/signup')
+        .send({
+          name: 'Jane Doe',
+          email: 'jane@mail.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        })
+        .expect(200)
+    })
   })
 })
