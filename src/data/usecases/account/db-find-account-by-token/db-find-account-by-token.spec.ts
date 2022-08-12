f133d312708887445751440fc1ce803ccb094088
Junior Paula <luke.junnior@icloud.com>
2022-08-12 20:04:14 -0300

    test: ensure DbFindAccountByToken returns null if Decrypter returns null

--- src/data/usecases/account/db-find-account-by-token/db-find-account-by-token.spec.ts
+++ src/data/usecases/account/db-find-account-by-token/db-find-account-by-token.spec.ts
@@ -29,7 +29,16 @@ describe('DbFindAccountByToken usecase', () => {
   test('Should call Decrypter with correct values', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.findByToken('any_token')
+    await sut.findByToken('any_token', 'any_role')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('Should returns null if Decrypter returns null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(
+      Promise.resolve(null)
+    )
+    const account = await sut.findByToken('any_token', 'any_role')
+    expect(account).toBeNull()
   })
 })
