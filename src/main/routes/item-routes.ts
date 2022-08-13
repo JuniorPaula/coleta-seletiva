170c582614d225c59094c936f544d2ff9e2ec850
Junior Paula <luke.junnior@icloud.com>
2022-08-13 12:35:23 -0300

    feat: ensure item route returns 200 if valid access token is provided

--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeGetItemController, makeItemController } from '../factories/item/item'
 import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/item', adminAuth, adaptRoute(makeItemController()))
-  router.get('/item', adaptRoute(makeGetItemController()))
+  router.get('/item', auth, adaptRoute(makeGetItemController()))
 }
