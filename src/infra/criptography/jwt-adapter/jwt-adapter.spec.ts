12409698530fc7ff1d46fbd7cb5722fef88ef5b5
Junior Paula <luke.junnior@icloud.com>
2022-08-13 09:59:43 -0300

    test: ensure JwtAdapter throw if virify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -52,5 +52,14 @@ describe('JwtAdapter', () => {
       const accessToken = await sut.decrypt('any_token')
       expect(accessToken).toBe('any_value')
     })
+
+    test('Should throw if virify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
