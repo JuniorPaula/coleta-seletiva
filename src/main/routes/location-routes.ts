f77d2e6f733174082e489dc5b0fb567df4443a76
Junior Paula <luke.junnior@icloud.com>
2022-08-13 12:41:01 -0300

    test: ensure location route return 403 if add location without access token

--- src/main/routes/location-routes.ts
+++ src/main/routes/location-routes.ts
@@ -3,9 +3,10 @@ import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeGetLocationController } from '../factories/location/get-location'
 import { makeLoadLocationByIdController } from '../factories/location/load-location-by-id-factory'
 import { makeLocationController } from '../factories/location/location'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  router.post('/location', adaptRoute(makeLocationController()))
+  router.post('/location', auth, adaptRoute(makeLocationController()))
   router.get('/location', adaptRoute(makeGetLocationController()))
   router.get('/location/:locationId', adaptRoute(makeLoadLocationByIdController()))
 }
