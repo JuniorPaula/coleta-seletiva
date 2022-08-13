1f5933a39322bee46a39f763c96e2821a9274fdf
Junior Paula <luke.junnior@icloud.com>
2022-08-13 12:05:12 -0300

    test: ensure add item route return 204 if  access token is provided

--- src/main/routes/item-routes.test.ts
+++ src/main/routes/item-routes.test.ts
@@ -1,9 +1,12 @@
-import { Collection } from 'mongodb'
 import request from 'supertest'
-import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import env from '../config/env'
 import app from '../config/app'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 let itemColletion: Collection
+let accountCollection: Collection
 
 describe('Item Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Item Routes', () => {
   beforeEach(async () => {
     itemColletion = MongoHelper.getCollection('items')
     await itemColletion.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   test('Should return 403 on add item whitout access token', async () => {
     await request(app)
@@ -26,5 +31,33 @@ describe('Item Routes', () => {
         image: 'oleo.png'
       })
       .expect(403)
+  })
+
+  test('Should return 204 on add item with valid access token', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Jane Doe',
+      email: 'jane.d@mail.com',
+      password: '1234',
+      role: 'admin'
+    })
+    const account = await accountCollection.findOne({ _id: res.insertedId })
+    const id = account._id
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        access_token: accessToken
+      }
+    })
+
+    await request(app)
+      .post('/api/v1/item')
+      .set('x-access-token', accessToken)
+      .send({
+        title: 'Oléo de cozinha',
+        image: 'oleo.png'
+      })
+      .expect(204)
   })
 })
--- src/main/routes/item-routes.ts
+++ src/main/routes/item-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import { adaptAuthMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeGetItemController, makeItemController } from '../factories/item/item'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-  const adminAuth = adaptRoute(makeAuthMiddleware('admin'))
+  const adminAuth = adaptAuthMiddleware(makeAuthMiddleware('admin'))
   router.post('/item', adminAuth, adaptRoute(makeItemController()))
   router.get('/item', adaptRoute(makeGetItemController()))
 }
